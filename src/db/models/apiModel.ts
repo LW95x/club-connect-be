e77979df2083fbc817433294ade5074763b9e4d5
LW95x <liam.woodall@live.co.uk>
2024-08-04 10:55:48 +0100

    moving endpoints.json to dist folder 3

--- src/db/models/apiModel.ts
+++ src/db/models/apiModel.ts
@@ -2,7 +2,6 @@ import fs  from "fs/promises";
 
 export const findApi = () => {
     const filePath = `${__dirname}/../../endpoints.json`;
-    console.log('Attempting to read file at path:', filePath);
     return fs.readFile(`${__dirname}/../../endpoints.json`, 'utf-8')
     .then((data) => {
         const parsedEndpoints = JSON.parse(data);
